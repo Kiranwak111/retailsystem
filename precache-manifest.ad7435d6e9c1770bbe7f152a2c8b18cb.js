ff90f32969701e88c886317d4ba6af711182833f
Kiranwak <kiranwak111@gmail.com>
2020-10-26 14:22:30 +0530

    Updates

---
+++ precache-manifest.ad7435d6e9c1770bbe7f152a2c8b18cb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91208b8c6d2849fea9cafe0fba80a2cd",
+    "url": "/retailsystem/index.html"
+  },
+  {
+    "revision": "fa1a01137cc98ad542bb",
+    "url": "/retailsystem/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "d91f82286758f7b90157",
+    "url": "/retailsystem/static/css/main.a0c60b9a.chunk.css"
+  },
+  {
+    "revision": "fa1a01137cc98ad542bb",
+    "url": "/retailsystem/static/js/2.18288914.chunk.js"
+  },
+  {
+    "revision": "a85f31d96a9e9d12f3382a067f2687ee",
+    "url": "/retailsystem/static/js/2.18288914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91f82286758f7b90157",
+    "url": "/retailsystem/static/js/main.873c5672.chunk.js"
+  },
+  {
+    "revision": "1623d453f8e7cdd97f07",
+    "url": "/retailsystem/static/js/runtime-main.020ac02e.js"
+  },
+  {
+    "revision": "c3309110ceeaabf33e14a12808fca617",
+    "url": "/retailsystem/static/media/0.c3309110.jpg"
+  },
+  {
+    "revision": "d549b90da0e52ab6ae2d027863abf5de",
+    "url": "/retailsystem/static/media/1.d549b90d.jpg"
+  },
+  {
+    "revision": "30cfef889b01440bcaad84ba601f2fd5",
+    "url": "/retailsystem/static/media/2.30cfef88.jpg"
+  },
+  {
+    "revision": "171a592ac580e85794045d677f3944db",
+    "url": "/retailsystem/static/media/3.171a592a.jpg"
+  },
+  {
+    "revision": "035af62c45a089156b5ad38d3370ffa7",
+    "url": "/retailsystem/static/media/4.035af62c.jpg"
+  },
+  {
+    "revision": "5af245d81bd370d983f0b18f3f6b364a",
+    "url": "/retailsystem/static/media/5.5af245d8.jpg"
+  },
+  {
+    "revision": "5b00968ce3b30fd06db94a0b21438fdc",
+    "url": "/retailsystem/static/media/6.5b00968c.jpg"
+  },
+  {
+    "revision": "0e1dc757e12755e86ea85f0dbd644d53",
+    "url": "/retailsystem/static/media/7.0e1dc757.jpg"
+  },
+  {
+    "revision": "a37cd5c8a3c4434fa616054de14368f1",
+    "url": "/retailsystem/static/media/8.a37cd5c8.jpg"
+  },
+  {
+    "revision": "ecf38463b10cfb2709247ec4d068a987",
+    "url": "/retailsystem/static/media/9.ecf38463.jpg"
+  }
+]);
